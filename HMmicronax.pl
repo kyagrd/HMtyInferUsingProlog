c1b39776d45f554b557ad15a56c0d5d623ece6ab
Ki Yung Ahn <kyagrd@gmail.com>
2015-08-20 19:17:21 +0100

    updated the text with bug fixes

--- HMmicronax.pl
+++ HMmicronax.pl
@@ -61,53 +61,35 @@ unfold_N_ap(N,E0$E1,E,Es) :-
 foldl_ap(E, []     , E).
 foldl_ap(E0,[E1|Es], E) :- foldl_ap(E0$E1, Es, E).
 
-%% samekinds is a helper predicate for inst_type
-samekinds(KC,[X|Xs],[Y|Ys],Gs,Gs1) :-
-  (X\==Y -> Gs0=[kind(KC,X,K),kind(KC,Y,K)|Gs]; Gs0=Gs),
-  samekinds(KC,Xs,Ys,Gs0,Gs1).
-samekinds(KC,[],[],Gs,Gs).
-
-%%%% T2 may be input (i.e. more speicific) so use fresh variable T1 for copy_term
-inst_type(KC,poly(C,T),T2,G,G1) :- copy_term(t(C,T),t(C,T1)), 
-  free_variables(T,Xs), free_variables(T1,Xs1), % Xs and Xs1 are same length
-  samekinds(KC,Xs,Xs1,G,G1), T1=T2. %% unify T1 T2 later. this trick only here once
-inst_type(KC,mono(T),T,G,G).
+first(X:T,[X1:T1|Zs]) :- X = X1 -> T = T1 ; first(X:T, Zs).
 
 instantiate(poly(C,T),T1) :- copy_term(t(C,T),t(C,T1)).
 instantiate(mono(T),T).
 
-first(X:T,[X1:T1|Zs]) :- X = X1 -> T = T1 ; first(X:T, Zs).
+inst_type(KC,poly(C,T),T2,G,G1) :- copy_term(t(C,T),t(C,T1)), 
+  free_variables(T,Xs), free_variables(T1,Xs1), % Xs and Xs1 are same length
+  samekinds(KC,Xs,Xs1,G,G1), T1=T2. %% unify T1 T2 later (T2 might not be var)
+inst_type(KC,mono(T),T,G,G).
+
+samekinds(KC,[X|Xs],[Y|Ys],Gs,Gs1) :- %  helper predicate for inst_type
+  (X\==Y -> Gs0=[kind(KC,X,K),kind(KC,Y,K)|Gs]; Gs0=Gs),
+  samekinds(KC,Xs,Ys,Gs0,Gs1).
+samekinds(KC,[],[],Gs,Gs).
 
 variablize(var(X)) :- gensym(t,X).
 
 infer_type(KC,C,E,T) :-
-  writef("here!!! 00 "), write(type(KC,C,E,T,[],Gs0)), nl,
   type(KC,C,E,T,[],Gs0), %%% handle delayed kind sanity check below
-  writef("here!!! AA"),nl,
-  writef("T = "), write(T), nl,
-  writef("KC = "), write(KC), nl,
-  writef("C = "), write(C), nl,
-  writef("Gs0 = "), write(Gs0), nl,
   copy_term(Gs0,Gs),
-  writef("Gs = "), write(Gs), nl,
   findall(Ty, member(kind(_,Ty,_),Gs), Tys),
   free_variables(Tys,Xs), maplist(variablize,Xs), % replace free tyvar to var(t)
-  findall(A:K,member(var(A),Xs),KC1), appendKC(Gs,KC1,Gs1), % extend with KC1 for new vars
-  writef("T = "), write(T), nl,
-  writef("C = "), write(C), nl,
-  writef("KC1 = "), write(KC1), nl,
-  writef("Gs1 = "), write(Gs1), nl,
-  writef("here!!! BB"),nl,
-  maplist(call,Gs1), % run all goals in Gs1
-  writef("here!!! CC"),nl,
-  true.
-
-
+  findall(A:K,member(var(A),Xs),KC1),
+  appendKC(Gs,KC1,Gs1), % extend with KC1 for new vars
+  maplist(call,Gs1). % run all goals in Gs1
 
 appendKC([],_,[]).
 appendKC([kind(KC,X,K)|Gs],KC1,[kind(KC2,X,K)|Gs1]) :-
   append(KC1,KC,KC2), appendKC(Gs,KC1,Gs1).
-
 
 % data B r a = BN | BC a (r(r a))
 % B : (* -> *) -> (* -> *)
--- HMtcpoly.pl
+++ HMtcpoly.pl
@@ -8,48 +8,45 @@ kind(KC,F $ G, K2) :- kind(KC,F,K1 -> K2), kind(KC,G,K1).
 kind(KC,A -> B,o)  :- kind(KC,A,o), kind(KC,B,o).
 
 type(KC,C,var(X),     T1,G,G1) :- first(X:T,C), inst_type(KC,T,T1,G,G1).
-type(KC,C,lam(X,E), A->B,G,G1) :- type(KC,[X:mono(A)  |C],E, B,G0,G1),
+type(KC,C,lam(X,E), A->B,G,G1) :- type(KC,[X:mono(A)|C],E,B,G0,G1),
                                   G0 = [kind(KC,A->B,o) | G]. % delay kind goal
 type(KC,C,X $ Y,       B,G,G1) :- type(KC,C,X,A->B,G, G0),
-                                  type(KC,C,Y,A,   G0,G1).
-type(KC,C,let(X=E0,E1),T,G,G1) :- type(KC,C,              E0,A,G, G0),
+                                  type(KC,C,Y,A,  G0,G1).
+type(KC,C,let(X=E0,E1),T,G,G1) :- type(KC,C,E0,A,G, G0),
                                   type(KC,[X:poly(C,A)|C],E1,T,G0,G1).
 
-zip([X|Xs],[Y|Ys],[(X,Y)|Zs]) :- zip(Xs,Ys,Zs).
-zip([],[],[]).
-
-inst_type(KC,poly(C,T),T1,G,G1) :- copy_term(t(C,T),t(C,T1)),
-  free_variables(T,Xs), free_variables(T1,Xs1), zip(Xs,Xs1,Ps),
-  findall([kind(KC,X,K),kind(KC,X1,K)],(member((X,X1),Ps),X\==X1),Gs),
-  flatten(Gs,G0), append(G0,G,G1).
-inst_type(KC,mono(T),T,G,G).
+first(X:T,[X1:T1|Zs]) :- X = X1 -> T = T1 ; first(X:T, Zs).
 
 instantiate(poly(C,T),T1) :- copy_term(t(C,T),t(C,T1)).
 instantiate(mono(T),T).
 
-first(X:T,[X1:T1|Zs]) :- X = X1 -> T = T1 ; first(X:T, Zs).
+inst_type(KC,poly(C,T),T2,G,G1) :- copy_term(t(C,T),t(C,T1)), 
+  free_variables(T,Xs), free_variables(T1,Xs1), % Xs and Xs1 are same length
+  samekinds(KC,Xs,Xs1,G,G1), T1=T2. %% unify T1 T2 later (T2 might not be var)
+inst_type(KC,mono(T),T,G,G).
+
+samekinds(KC,[X|Xs],[Y|Ys],Gs,Gs1) :- %  helper predicate for inst_type
+  (X\==Y -> Gs0=[kind(KC,X,K),kind(KC,Y,K)|Gs]; Gs0=Gs),
+  samekinds(KC,Xs,Ys,Gs0,Gs1).
+samekinds(KC,[],[],Gs,Gs).
 
 variablize(var(X)) :- gensym(t,X).
 
 infer_type(KC,C,E,T) :-
-  type(KC,C,E,T,[],Gs0), % handle delayed kind sanity check below
+  type(KC,C,E,T,[],Gs0), %%% handle delayed kind sanity check below
   copy_term(Gs0,Gs),
   findall(Ty, member(kind(_,Ty,_),Gs), Tys),
-  free_variables(Tys,Xs),
-  maplist(variablize,Xs), % replace free tyvar to var(t)
+  free_variables(Tys,Xs), maplist(variablize,Xs), % replace free tyvar to var(t)
   findall(A:K,member(var(A),Xs),KC1),
   appendKC(Gs,KC1,Gs1), % extend with KC1 for new vars
-  maplist(call,Gs1), % run all goals in Gs1
-%%%% debugging output
-%%  writef("T = "), write(T), nl,
-%%  writef("C = "), write(C), nl,
-%%  writef("KC1 = "), write(KC1), nl,
-%%  writef("Gs1 = "), write(Gs1), nl,
-  true.
+  maplist(call,Gs1). % run all goals in Gs1
 
 appendKC([],_,[]).
 appendKC([kind(KC,X,K)|Gs],KC1,[kind(KC2,X,K)|Gs1]) :-
   append(KC1,KC,KC2), appendKC(Gs,KC1,Gs1).
+
+
+
 
 ctx0([ 'Nat':mono(o)
      , 'List':mono(o->o)
