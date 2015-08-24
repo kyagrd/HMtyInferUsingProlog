98db181b1b613497d00ec3ece7ceb3f7f7a491d5
Ki Yung Ahn <kyagrd@gmail.com>
2015-08-24 09:35:46 +0100

    updating code with no cut and DCG

--- HMnocut.pl
+++ HMnocut.pl
@@ -1,16 +1,16 @@
 :- set_prolog_flag(occurs_check,true).
 :- op(500,yfx,$).
 
-type(C,var(X),T1) :- first(X:T,C),instantiate(T,T1).
-type(C,lam(X,E),A -> B) :- type([X:mono(A)|C],E,B).
-type(C,X $ Y,B) :- type(C,X,A -> B), type(C,Y,A).
-type(C,let(X = E0,E1),T) :- type(C,E0,A),type([X:poly(C,A)|C],E1,T).
+type(C,var(X),       T1) :- first(X:T,C), instantiate(T,T1).
+type(C,lam(X,E), A -> B) :- type([X:mono(A)|C],E,B).
+type(C,X $ Y,        B ) :- type(C,X,A -> B), type(C,Y,A).
+type(C,let(X=E0,E1), T ) :- type(C,E0,A), type([X:poly(C,A)|C],E1,T).
+
+first(K:V,[K1:V1|Xs]) :- K = K1, V=V1.
+first(K:V,[K1:V1|Xs]) :- K\==K1, first(K:V, Xs).
 
 instantiate(poly(C,T),T1) :- copy_term(t(C,T),t(C,T1)).
 instantiate(mono(T),T).
-
-first(K:V,[K1:V1|Xs]) :- K=K1, V=V1.
-first(K:V,[K1:V1|Xs]) :- K\==K1, first(K:V, Xs). % no cut but using var cmp
 
 main(T) :-
   X = var(x), Y = var(y), Z = var(z),
